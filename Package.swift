724f8982318fdf5711d3543daa0ca29584aed915
Georgii <georgii@percents.com>
2023-02-23 10:59:49 -0500

    Done

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "PercentsRewards",
+    name: "TestPackage",
     defaultLocalization: "en",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PercentsRewards",
-            targets: ["PercentsRewards"]),
+            name: "TestPackage",
+            targets: ["TestPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,10 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PercentsRewards",
+            name: "TestPackage",
             dependencies: []),
         .testTarget(
-            name: "PercentsRewardsTests",
-            dependencies: ["PercentsRewards"]),
+            name: "TestPackageTests",
+            dependencies: ["TestPackage"]),
     ]
 )
